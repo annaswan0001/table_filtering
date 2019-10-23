26d2cf73531961aa8b97442dc13ccd2108bc3183
A.Kyzmenko <a.kyzmenko@MacBook-Air-Anna.local>
2019-10-23 12:30:43 +0400

    add buttons-selection size of the table

--- src/App.js
+++ src/App.js
@@ -4,69 +4,83 @@ import Table from "./Table";
 import DetaiRowView from "./DetailRowView";
 import _ from "lodash";
 import "./index.css";
+import ModeSelector from "./ModeSelector";
+import ReactPaginate from 'react-paginate'; 
 
 class App extends React.Component {
-  state = {
-    loader: true,
-    data: [],
-    sort: "asc",
-    sortField: "id",
-    row: ""
-  };
-
-  onSort = sortField => {
-    const cloneData = this.state.data.concat(); //clone data
-    const sortType = this.state.sort === "asc" ? "desc" : "asc"; //sort direction
-    const orderedData = _.orderBy(cloneData, sortField, sortType);
-
-    this.setState({
-      data: orderedData,
-      sort: sortType,
-      sortField
-    });
-  };
-
-  onRowSelect = row => {
-    this.setState({
-      row
-    });
-  };
-  async componentDidMount() {
-    try {
-      const res = await fetch(
-        ` http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`
-      );
-      const data = await res.json();
-      this.setState({
+    state = {
+        isModeSelected: false,
         loader: false,
-        data: _.orderBy(data, this.state.sortField, this.state.sort)
-      });
-      console.log(data);
-    } catch (e) {
-      console.log(e);
-    }
-  }
+        data: [],
+        sort: "asc",
+        sortField: "id",
+        row: ""
+    };
 
-  render() {
-    const { row, loader, data } = this.state;
-    return (
-      <div className="container">
-        <h1>Simple sorting</h1>
-        {loader ? (
-          <Loader />
-        ) : (
-          <Table
-            onSort={this.onSort}
-            data={data}
-            sort={this.state.sort}
-            sortField={this.state.sortField}
-            onRowSelect={this.onRowSelect}
-          />
-        )}
-        <DetaiRowView person={row} />
-      </div>
-    );
-  }
+    onSort = sortField => {
+        const cloneData = this.state.data.concat(); //clone data
+        const sort = this.state.sort === "asc" ? "desc" : "asc"; //sort direction
+        const data = _.orderBy(cloneData, sortField, sort);
+
+        this.setState({
+            data,
+            sort,
+            sortField
+        });
+    };
+
+    onRowSelect = row => {
+        this.setState({
+            row
+        });
+    };
+
+    onSelect = url => {
+        this.setState({
+            loader: true,
+            isModeSelected: true
+        });
+        this.fetchData(url);
+    };
+
+    async fetchData(url) {
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            this.setState({
+                loader: false,
+                data: _.orderBy(data, this.state.sortField, this.state.sort)
+            });
+            console.log(data);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    render() {
+        const { row, loader, data } = this.state;
+        return (
+            <div className="container">
+                {!this.state.isModeSelected ? (
+                    <ModeSelector onSelect={this.onSelect} />
+                ) : loader ? (
+                    <Loader />
+                ) : (
+                    <React.Fragment>
+                        <ModeSelector onSelect={this.onSelect} />
+                        <Table
+                            onSort={this.onSort}
+                            data={data}
+                            sort={this.state.sort}
+                            sortField={this.state.sortField}
+                            onRowSelect={this.onRowSelect}
+                        />
+                    </React.Fragment>
+                )}
+                <DetaiRowView person={row} />
+            </div>
+        );
+    }
 }
 
 export default App;
