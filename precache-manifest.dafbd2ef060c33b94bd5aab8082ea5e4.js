a58dc96f29cd611b5e9152ea0a80b27ed80cb32a
Anna Swan <42541919+annaswan0001@users.noreply.github.com>
2020-05-07 15:44:33 +0300

    Updates

---
+++ precache-manifest.dafbd2ef060c33b94bd5aab8082ea5e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff7e1cbe25e677dd3edafb14cb410192",
+    "url": "/table_filtering/index.html"
+  },
+  {
+    "revision": "702a2a71f432163bd416",
+    "url": "/table_filtering/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "253dbf2470d6b467185d",
+    "url": "/table_filtering/static/css/main.77918d88.chunk.css"
+  },
+  {
+    "revision": "702a2a71f432163bd416",
+    "url": "/table_filtering/static/js/2.cf9ff11a.chunk.js"
+  },
+  {
+    "revision": "253dbf2470d6b467185d",
+    "url": "/table_filtering/static/js/main.9de49898.chunk.js"
+  },
+  {
+    "revision": "9f5a646b162bcd877227",
+    "url": "/table_filtering/static/js/runtime-main.3aa663a9.js"
+  }
+]);
